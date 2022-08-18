9375c961430a104fca783864fd2673d79930d25e
FerMendoza123 <caifanes_10@live.com.mx>
2022-08-17 22:18:04 -0500

    Vistas (vacias) y rutas agregadas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function propiertiesList()
+    {
+        return view("propiertiesList");
+    }
+
+    public function propiertyPage($idProp)
+    {
+        
+    }
 }
